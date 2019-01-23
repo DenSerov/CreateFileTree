330c6b63772bdab9213f1b113320b51199ca945b
DenSerov <35998789+DenSerov@users.noreply.github.com>
2019-01-23 21:46:32 +0300

    added configuration variables for tree definition

--- create_tree.py
+++ create_tree.py
@@ -6,7 +6,7 @@ from multiprocessing import Process
 def make_branch(dir_l0,n):
     total=0
     ldir='long_directory_name_long_directory_name_long_directory_name_long_directory_name_long_directory_name'
-    dir_start_str='C:/tmp/dir10m/'+dir_l0+str(n)+'/'
+    dir_start_str='C:/tmp/'+dir_l0+str(n)+'/'
     os.mkdir(dir_start_str)
 
     for i in range(10):
@@ -23,24 +23,26 @@ def make_branch(dir_l0,n):
                 for l in range(10):
                     dir_l4=ldir+str(l)+'/'
                     os.mkdir(dir_start_str+dir_l1+dir_l2+dir_l3+dir_l4)
-                    for m in range(100):
+                    for m in range(10):
                         total+=1
                         fname='long_file_name_long_file_name_long_file_name_long_file_name_long_file_name_long_file_name_long_file_name.'+str(m)
-                        #os.system('touch '+dir_start_str+dir_l1+dir_l2+dir_l3+dir_l                                                                                                                                                             4+fname3#
                         f=open(dir_start_str+dir_l1+dir_l2+dir_l3+dir_l4+fname,"w")
                         f.close()
-                        
+
     return total
 
 start=time.time()
-#print(make_branch('dir',2),'files created.')
-#print(round(time.time()-start),2)
-#print('The end.')
-#exit()
+dir_start='c:/tmp/'
+dirname_template='dir'
+filename_template='file'
+dir_depth=4
+files_per_dir=10
+threads=1
+
 print(__name__)
 if __name__ == '__main__':
     procs=[]
-    branches=int(sys.argv[1])
+    branches=threads
     #branches=10
     for n in range(branches):
         proc = Process(target=make_branch, args=('dir',n))
